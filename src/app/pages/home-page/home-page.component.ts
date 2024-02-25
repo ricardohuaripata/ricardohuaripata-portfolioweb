eef1b9b39f1ae7dd693c04494ad2028033a8fba7
Ricardo Huaripata <102578959+ricardohuaripata@users.noreply.github.com>
2024-02-25 21:53:24 +0100

    emailjs implemented

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -8,15 +8,22 @@ import {
   Validators,
 } from '@angular/forms';
 import { TechnologiesComponent } from '../../components/technologies/technologies.component';
-import { ProjectsComponent } from "../../components/projects/projects.component";
-import { FooterComponent } from "../../components/footer/footer.component";
+import { ProjectsComponent } from '../../components/projects/projects.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
-    selector: 'app-home-page',
-    standalone: true,
-    templateUrl: './home-page.component.html',
-    styleUrl: './home-page.component.scss',
-    imports: [ReactiveFormsModule, CommonModule, TechnologiesComponent, ProjectsComponent, FooterComponent]
+  selector: 'app-home-page',
+  standalone: true,
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss',
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    TechnologiesComponent,
+    ProjectsComponent,
+    FooterComponent,
+  ],
 })
 export class HomePageComponent {
   form: FormGroup;
@@ -29,7 +36,8 @@ export class HomePageComponent {
       empresa: 'Atos España',
       enlace: 'https://atos.net/es/spain',
       fecha: 'Marzo 2023 - Junio 2023',
-      descripcion: 'Estancia en empresa de consultoría formando parte de un equipo de QA trabajando en el control de calidad del frontend de una aplicación web mediante pruebas automatizadas. Las tecnologías llevadas a cabo para la automatización de pruebas fueron Selenium en un proyecto Spring Boot, Git para el control de versiones, Trello y Github para la gestión de tareas y Scrum como metodología de desarrollo.'
+      descripcion:
+        'Estancia en empresa de consultoría formando parte de un equipo de QA trabajando en el control de calidad del frontend de una aplicación web mediante pruebas automatizadas. Las tecnologías llevadas a cabo para la automatización de pruebas fueron Selenium en un proyecto Spring Boot, Git para el control de versiones, Trello y Github para la gestión de tareas y Scrum como metodología de desarrollo.',
     },
   ];
   constructor(private fb: FormBuilder) {
@@ -52,7 +60,7 @@ export class HomePageComponent {
     return null;
   }
 
-  onSubmit(): void {
+  async onSubmit(): Promise<void> {
     this.submited = true;
     if (this.form.invalid) {
       return;
@@ -60,11 +68,22 @@ export class HomePageComponent {
 
     this.loading = true;
 
-    setTimeout(() => {
-      this.form.reset();
-      this.submited = false;
+    emailjs.init('T9GCaCcJtOv0DqMFT');
+
+    let response = await emailjs.send('service_uzu2m04', 'template_ss4qu9k', {
+      to_name: 'Ricardo',
+      from_name: this.form.get('name')!.value,
+      from_email: this.form.get('email')!.value,
+      subject: this.form.get('subject')!.value,
+      message: this.form.get('message')!.value,
+    });
+
+    this.form.reset();
+    this.submited = false;
+    this.loading = false;
+
+    if (response.status == 200) {
       this.successSubmit = true;
-      this.loading = false;
-    }, 1000);
+    }
   }
 }
