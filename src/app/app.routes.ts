b288ab461b2b0c23eb70f1844529aca698b3681d
Ricardo Huaripata <102578959+ricardohuaripata@users.noreply.github.com>
2024-10-06 20:54:18 +0200

    optimize images

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -25,35 +25,35 @@ const projects: Project[] = [
       'Bootstrap',
     ],
     githubRepoUrl: 'https://github.com/ricardohuaripata/ecommerce-frontend',
-    thumbnail: 'https://res.cloudinary.com/dmravgyts/image/upload/v1727969994/projects/ogs-ecommerce/project2-thumbnail_k3l7sy.png',
+    thumbnail: 'https://res.cloudinary.com/dmravgyts/image/upload/v1728236435/projects/ogs-ecommerce/project2-thumbnail-min_jnhppq.png',
     gallery: [
       {
-        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1727703510/projects/ogs-ecommerce/photos/828_1x_shots_so_afhqrq.png',
+        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1728238996/projects/ogs-ecommerce/photos/828_1x_shots_so_afhqrq-min_fxpyrm.png',
         width: 1920,
         height: 1280,
       },
       {
-        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1727702548/projects/ogs-ecommerce/photos/701shots_so_rmobsq.png',
+        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1728238995/projects/ogs-ecommerce/photos/701shots_so_rmobsq-min_hzogzo.png',
         width: 1920,
         height: 1280,
       },
       {
-        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1727702542/projects/ogs-ecommerce/photos/94shots_so_eyqgie.png',
+        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1728238988/projects/ogs-ecommerce/photos/94shots_so_eyqgie-min_rrpnru.png',
         width: 1920,
         height: 1280,
       },
       {
-        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1727703991/projects/ogs-ecommerce/photos/192_1x_shots_so_rkvnpx.png',
+        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1728238990/projects/ogs-ecommerce/photos/192_1x_shots_so_rkvnpx-min_user85.png',
         width: 1920,
         height: 1280,
       },
       {
-        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1727710604/projects/ogs-ecommerce/photos/170_1x_shots_so_wfzuzo.png',
+        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1728238988/projects/ogs-ecommerce/photos/170_1x_shots_so_wfzuzo-min_tfdos6.png',
         width: 1920,
         height: 1280,
       },
       {
-        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1727710598/projects/ogs-ecommerce/photos/324_1x_shots_so_dajw6r.png',
+        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1728238993/projects/ogs-ecommerce/photos/324_1x_shots_so_dajw6r-min_wgjghj.png',
         width: 1920,
         height: 1280,
       },
@@ -62,19 +62,19 @@ const projects: Project[] = [
       {
         title: 'Añadir al carrito',
         thumbnail:
-          'https://res.cloudinary.com/dmravgyts/image/upload/v1727701943/projects/ogs-ecommerce/photos/260_1x_shots_so_lltfsy.png',
+          'https://res.cloudinary.com/dmravgyts/image/upload/v1728238361/projects/ogs-ecommerce/photos/260_1x_shots_so_min_d9coe0.png',
         link: 'https://res.cloudinary.com/dmravgyts/video/upload/v1724008937/projects/ogs-ecommerce/videos/E-commerce-Demo_a6fah7.mp4',
       },
       {
         title: 'Verificación de email',
         thumbnail:
-          'https://res.cloudinary.com/dmravgyts/image/upload/v1727701120/projects/ogs-ecommerce/photos/626_2x_shots_so_bydigo.png',
+          'https://res.cloudinary.com/dmravgyts/image/upload/v1728238358/projects/ogs-ecommerce/photos/626_2x_shots_so_min_qsq8z2.png',
         link: 'https://res.cloudinary.com/dmravgyts/video/upload/v1723991809/projects/ogs-ecommerce/videos/E-commerce_-_Email_verification_brjysd.mp4',
       },
       {
         title: 'Cuenta del usuario',
         thumbnail:
-          'https://res.cloudinary.com/dmravgyts/image/upload/v1727701573/projects/ogs-ecommerce/photos/590_2x_shots_so_ovddno.png',
+          'https://res.cloudinary.com/dmravgyts/image/upload/v1728238362/projects/ogs-ecommerce/photos/590_2x_shots_so_min_hhrieo.png',
         link: 'https://res.cloudinary.com/dmravgyts/video/upload/v1724001903/projects/ogs-ecommerce/videos/E-commerce_-_Account_ultppi.mp4',
       },
     ],
@@ -128,35 +128,35 @@ const projects: Project[] = [
       'Bootstrap',
     ],
     githubRepoUrl: 'https://github.com/ricardohuaripata/ASHON-Ecommerce-Client',
-    thumbnail: 'https://res.cloudinary.com/dmravgyts/image/upload/v1727970004/projects/ashon-ecommerce/project1-thumbnail_xbxmkw.png',
+    thumbnail: 'https://res.cloudinary.com/dmravgyts/image/upload/v1728236749/projects/ashon-ecommerce/project1-thumbnail-min_xdmmaz.png',
     gallery: [
       {
-        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1727708182/projects/ashon-ecommerce/photos/981_1x_shots_so_oqyywk.png',
+        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1728240483/projects/ashon-ecommerce/photos/981_1x_shots_so_min_ubzrls.png',
         width: 1920,
         height: 1280,
       },
       {
-        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1727709489/projects/ashon-ecommerce/photos/51_1x_shots_so_gyrkph.png',
+        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1728240476/projects/ashon-ecommerce/photos/51_1x_shots_so_min_nfxbmu.png',
         width: 1920,
         height: 1280,
       },
       {
-        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1727709502/projects/ashon-ecommerce/photos/775shots_so_esebkz.png',
+        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1728240475/projects/ashon-ecommerce/photos/6shots_so-min_exqgfo.png',
         width: 1920,
         height: 1280,
       },
       {
-        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1727709513/projects/ashon-ecommerce/photos/773_1x_shots_so_pjizbo.png',
+        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1728240482/projects/ashon-ecommerce/photos/773_1x_shots_so_min_uhtw18.png',
         width: 1920,
         height: 1280,
       },
       {
-        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1727709519/projects/ashon-ecommerce/photos/654_1x_shots_so_tmajoe.png',
+        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1728240480/projects/ashon-ecommerce/photos/654_1x_shots_so_min_it0qbg.png',
         width: 1920,
         height: 1280,
       },
       {
-        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1727709524/projects/ashon-ecommerce/photos/224_1x_shots_so_vudn8n.png',
+        src: 'https://res.cloudinary.com/dmravgyts/image/upload/v1728240478/projects/ashon-ecommerce/photos/224_1x_shots_so_min_yy7xuv.png',
         width: 1920,
         height: 1280,
       },
@@ -165,19 +165,19 @@ const projects: Project[] = [
       {
         title: 'Añadir al carrito',
         thumbnail:
-          'https://res.cloudinary.com/dmravgyts/image/upload/v1727707758/projects/ashon-ecommerce/photos/263_1x_shots_so_auwf7y.png',
+          'https://res.cloudinary.com/dmravgyts/image/upload/v1728239665/projects/ashon-ecommerce/photos/263_1x_shots_so_min_cs1wgh.png',
         link: 'https://res.cloudinary.com/dmravgyts/video/upload/v1708714309/projects/ashon-ecommerce/videos/ASHON-Demo-Add-to-cart_gr4zjr.mp4',
       },
       {
         title: 'Google login',
         thumbnail:
-          'https://res.cloudinary.com/dmravgyts/image/upload/v1727707774/projects/ashon-ecommerce/photos/477_1x_shots_so_pkrvar.png',
+          'https://res.cloudinary.com/dmravgyts/image/upload/v1728239664/projects/ashon-ecommerce/photos/477_1x_shots_so_min_ry0xbs.png',
         link: 'https://res.cloudinary.com/dmravgyts/video/upload/v1723992581/projects/ashon-ecommerce/videos/ASHON_Store_-_Google_Login_Hecho_con_Clipchamp_tjier7.mp4',
       },
       {
         title: 'Verificación de email',
         thumbnail:
-          'https://res.cloudinary.com/dmravgyts/image/upload/v1727707770/projects/ashon-ecommerce/photos/716_1x_shots_so_z7ozx9.png',
+          'https://res.cloudinary.com/dmravgyts/image/upload/v1728239667/projects/ashon-ecommerce/photos/716_1x_shots_so_min_ly9ndd.png',
         link: 'https://res.cloudinary.com/dmravgyts/video/upload/v1723991572/projects/ashon-ecommerce/videos/ashon-email-verification_qefr5b.mp4',
       },
     ],
@@ -223,7 +223,7 @@ const projects: Project[] = [
     category: 'Fullstack',
     tags: ['Angular', 'Bootstrap', 'Supabase'],
     githubRepoUrl: 'https://github.com/ricardohuaripata/4-fotos-1-palabra',
-    thumbnail: 'https://res.cloudinary.com/dmravgyts/image/upload/v1727969957/projects/4f1p/project3-thumbnail_fklm8e.png',
+    thumbnail: 'https://res.cloudinary.com/dmravgyts/image/upload/v1728237266/projects/4f1p/project3-thumbnail-min_gghzxx.png',
     link: 'https://github.com/ricardohuaripata/4-fotos-1-palabra',
   },
   {
@@ -233,7 +233,7 @@ const projects: Project[] = [
     category: 'Frontend',
     tags: ['Angular', 'Tailwind CSS', 'Flowbite', 'Swiper'],
     githubRepoUrl: 'https://github.com/ricardohuaripata/cinesa-web',
-    thumbnail: 'https://res.cloudinary.com/dmravgyts/image/upload/v1727969966/projects/cinesa/project4-thumbnail_lm8qgn.png',
+    thumbnail: 'https://res.cloudinary.com/dmravgyts/image/upload/v1728236952/projects/cinesa/project4-thumbnail-min_u2sgza.png',
     link: 'https://cinesaweb.netlify.app/',
   },
   {
@@ -243,7 +243,7 @@ const projects: Project[] = [
     category: 'Frontend',
     tags: ['Angular', 'Tailwind CSS', 'Flowbite'],
     githubRepoUrl: 'https://github.com/ricardohuaripata/vanquish-fitness',
-    thumbnail: 'https://res.cloudinary.com/dmravgyts/image/upload/v1727969926/projects/vqfit/project5-thumbnail_raapsg.png',
+    thumbnail: 'https://res.cloudinary.com/dmravgyts/image/upload/v1728237151/projects/vqfit/project5-thumbnail-min_ia5c6n.png',
     link: 'https://vqfit.netlify.app/',
   },
 ];
